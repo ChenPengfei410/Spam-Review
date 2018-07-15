f68a9589ce11a785aacadfff0618a4bd5f2f82df
唐恺辰 <13701374546@139.com>
2018-07-15 11:12:28 +0800

    Add files via upload

---
+++ HMM/HMM.py
@@ -0,0 +1,47 @@
+from hmmlearn.hmm import GaussianHMM
+import datetime
+import numpy as np
+import warnings
+
+
+def interval(date_list):
+	result = list()
+	for i in range(1, len(date_list)):
+		result.append([(date_list[i] - date_list[i - 1]).days])
+	return result
+
+
+X = list()
+lengths = list()
+user_id_list = list()
+with open("sorted.txt", 'r') as file:
+	for line in file:
+		user_id_list.append(line)
+		length = int(file.readline())
+		review_time = list()
+		for i in range(length):
+			info = file.readline().split('\t')
+			review_time.append(datetime.datetime(int(info[3][:4]), int(info[3][5:7]), int(info[3][8:10])))
+		X.append(interval(review_time))
+		lengths.append(length - 1)
+		file.readline()
+X = np.concatenate(X)
+print(X)
+
+warnings.filterwarnings("ignore")
+model = GaussianHMM(n_components=20, n_iter=10000, tol=1, verbose=True)
+model.fit(X, lengths)
+if model.monitor_.converged:
+	print(model.transmat_)
+	print(model.means_)
+	print(model.covars_)
+hidden_state = model.predict(X, lengths)
+
+start = 0
+with open("hidden_states.txt", 'w') as file:
+	for i in range(len(user_id_list)):
+		file.write(user_id_list[i] + '\n')
+		for j in range(lengths[i]):
+			file.write(str(hidden_state[start + j]) + '\t')
+		start += lengths[i]
+		file.write('\n')
---
+++ HMM/readFile.py
@@ -0,0 +1,37 @@
+import datetime
+import operator
+
+
+person_dict = dict()
+with open("metadata", 'r') as file:
+	for line in file:
+		line = line.split('\t')
+		user_id = line[0]
+		user_info = dict()
+		user_info["prod_id"] = int(line[1])
+		user_info["rating"] = int(float(line[2]))
+		user_info["label"] = int(line[3])
+		user_info["date"] = datetime.datetime(int(line[4][:4]), int(line[4][5:7]), int(line[4][8:10]))
+		if person_dict.__contains__(user_id):
+			person_dict[user_id].append(user_info)
+		else:
+			person_dict[user_id] = list()
+
+for user_id in person_dict.keys():
+	person_dict[user_id] = sorted(person_dict[user_id], key=operator.itemgetter('date'))
+
+threshold = 10
+record_counter = reviewer_counter = 0
+with open("sorted.txt", 'w') as file:
+	for user_id in person_dict.keys():
+		length = len(person_dict[user_id])
+		if length < threshold:
+			continue
+		record_counter += length
+		reviewer_counter += 1
+		file.write(user_id + '\n' + str(length) + '\n')
+		for info in person_dict[user_id]:
+			file.write(str(info["prod_id"]) + '\t' + str(info["rating"]) + '\t' + str(info["label"]) + '\t' +
+					   info["date"].strftime("%Y-%m-%d") + '\n')
+		file.write('\n')
+print(record_counter, reviewer_counter)
